f85dbc681a97843193f6002a057be57bf1b21e77
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-16 14:18:19 -0300

    build: structure of gem with Rubymine

---
+++ lib/mystudy_rubymine_gem.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mystudy_rubymine_gem/version"
+
+module MystudyRubymineGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mystudy_rubymine_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MystudyRubymineGem do
+  it "has a version number" do
+    expect(MystudyRubymineGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
