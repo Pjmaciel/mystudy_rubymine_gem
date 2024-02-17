a49803e7f8920e2a1edac3e6fe5f7854868e74f4
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-16 22:29:51 -0300

    test: add When non of a number and hen the input negative number

--- lib/searching_algorithms/linear_search.rb
+++ lib/searching_algorithms/linear_search.rb
@@ -1,30 +1,20 @@
-# frozen_string_literal: true
-
-# Classe para realizar uma busca linear em uma lista de números.
 class LinearSearch
   attr_accessor :choice_number, :number_list
 
-  # Inicializa a classe com o número a ser procurado e a lista de números.
-  #
-  # @param choice_number [Integer] O número a ser procurado na lista.
-  # @param number_list [Array<Integer>] A lista de números onde será realizada a busca.
-  def initialize
-    @choice_number = nil
-    @number_list = []
+  def valid_choice?
+    choice_number.is_a?(Integer) && choice_number > 0
   end
 
-  # Realiza uma busca linear na lista de números para verificar se o número está presente.
-  #
-  # @return [Boolean] Retorna true se o número estiver presente na lista, caso contrário, false.
   def searching
-    found = false
-    number_list.each do |number|
-      if number == choice_number
-        found = true
-        break
-      end
-    end
+    return false unless valid_choice?
 
-    found
+    number_list.each do |number|
+      return true if number == choice_number
+    end
+    false
   end
+
+  private
+
+  attr_reader :choice_number, :number_list
 end
