34751249ad68ec9cbf9f6f39ee357ba6e1e86001
Pjmaciel <pjmaciel_4@hotmail.com>
2024-02-19 15:25:30 -0300

    test: when the number exists and when non exists in the list

--- lib/searching_algorithms/binary_search.rb
+++ lib/searching_algorithms/binary_search.rb
@@ -9,21 +9,23 @@ class BinarySearch
   end
 
   def searching(choice_number, list_numbers)
+
+    list_numbers_sorted = list_numbers.sort
     first_index = 0
-    last_index = list_numbers.length - 1
+    last_index = list_numbers_sorted .length - 1
 
     while first_index <= last_index
       mid_index = (first_index + last_index) / 2
 
-      if list_numbers[mid_index] == choice_number
+      if list_numbers_sorted[mid_index] == choice_number
         puts "#{choice_number} foi encontrado na lista"
         return mid_index
-      elsif list_numbers[mid_index] < choice_number
+      elsif list_numbers_sorted[mid_index] < choice_number
         first_index = mid_index + 1
       else
         last_index = mid_index - 1
       end
     end
-    return -1
+    -1
   end
 end
